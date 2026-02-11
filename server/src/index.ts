7635b964dfd08d6a35bfcaea25830b7d45576ded
OleksiiMishchenko <alex.mishch@gmail.com>
2026-02-11 17:58:41 +0200

    init: setup monorepo with server (graphql-yoga) and client (react + vite + tailwind)

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import { createServer } from 'node:http';
+import { createYoga, createSchema } from 'graphql-yoga';
+
+const typeDefs = /* GraphQL */ `
+  type Query {
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from GraphQL server 🚀'
+  }
+}
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs,
+    resolvers
+  })
+})
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.log('Server is running on http://localhost:4000/graphql')
+})
